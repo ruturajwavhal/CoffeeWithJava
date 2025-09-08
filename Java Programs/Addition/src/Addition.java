de25eb1936d7fa40a9bf0121781ac1a73aa812f5
unknown <ruturajwavhal10@gmail.com>
2025-09-08 11:52:47 +0530

    .gitignore file has updated

--- Java Programs/Addition/src/Addition.java
+++ Java Programs/Addition/src/Addition.java
@@ -1,8 +1,11 @@
 public class Addition {
     public static void main(String [] args)
     {
-        int a=10;
+        /*
+        Addition of two numbers
+         */
+        int a = 10;
         int b = 20;
-        System.out.println("Addition of "+a+" and "+b+" is "+(a+b));
+        System.out.println("Addition of "+ a +" and "+ b +" is "+ (a + b));
     }
 }
--- Java Programs/BankProgram/src/Bank.java
+++ Java Programs/BankProgram/src/Bank.java
@@ -2,22 +2,28 @@ public class Bank {
     String accHolderName;
     int accNum;
     int currentBalance;
+/*
+We assigning values to the datamember of class Using Paramaterized Constructor
+ */
 
-// We assigning values to the datamember of class Using Paramaterized Constructor
     public Bank(String accHolderName, int accNum, int currentBalance)
     {
         this.accHolderName = accHolderName;
         this.accNum = accNum;
         this.currentBalance = currentBalance;
     }
+/*
+In this method we printing current account balance
+ */
 
-// In this method we printing current account balance
     public void getAccBalance()
     {
         System.out.println("Current Balance = "+ currentBalance);
     }
+/*
+In this method we printing account holder details
+ */
 
-// In this method we printing account holder details
     public void getAccDetails()
     {
         System.out.println("Account Holder Name : " + accHolderName + "\nAccount Number : " + accNum + "\nCurrent Balance : " + currentBalance);

47a6b129543322108b0aae8b8225ed50939e3a9e
unknown <ruturajwavhal10@gmail.com>
2025-09-08 12:36:24 +0530

    project files are deleted

--- Java Programs/BankProgram/src/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String args[])
-    {
-        Bank b = new Bank("Suraj Bhalerao", 987324, 1000);
-        b.getAccDetails();
-        System.out.println("\n------------------------\n");
-        b.getAccBalance();
-    }
-}
--- Java Programs/Substraction/src/Substraction.java
+++
@@ -1,8 +0,0 @@
-public class Substraction {
-    public static void main(String args[])
-    {
-        int a =40;
-        int b =30;
-        System.out.println("Substraction of "+a+" and "+b+" is "+(a-b));
-    }
-}

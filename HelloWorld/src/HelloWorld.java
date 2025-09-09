2f7e09c5ce298c4a08e36053685fd4e424974ba2
unknown <ruturajwavhal10@gmail.com>
2025-09-09 21:08:11 +0530

    Hello World program is updated

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        /**
+        Printing hello world
+         */
+        System.out.println("Hello World");
+    }
+}

bcaf9497266351ac2c29047e48c04e1f9d257a24
unknown <ruturajwavhal10@gmail.com>
2025-09-09 21:21:30 +0530

    Substratcion program is modified

--- Substraction/src/Substraction.java
+++ Substraction/src/Substraction.java
@@ -6,6 +6,6 @@ public class Substraction {
          */
         int a = 40;
         int b = 30;
-        System.out.println("Substraction of " + a + " and " + b + " is " + (a - b));
+        System.out.println("Substraction of " + a + " and " + b + " is : " + (a - b));
     }
 }

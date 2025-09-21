36ce83af1487863d8959d54629aac990097712ad
unknown <ruturajwavhal10@gmail.com>
2025-09-21 11:00:24 +0530

    Do while programs added

---
+++ DoWhilePrograms/src/DoWhileProgram.java
@@ -0,0 +1,11 @@
+public class DoWhileProgram {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+        do{
+            sum += i;
+            i++;
+        }while(i <= 5);
+        System.out.println("Sum of numbers from 1 to 5 is : " + sum);
+    }
+}

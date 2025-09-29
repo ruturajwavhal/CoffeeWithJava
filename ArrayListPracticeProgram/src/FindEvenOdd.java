ee6979860a55760f10a772b5b0179e7bfacc7979
Ruturaj <99982142+ruturajwavhal@users.noreply.github.com>
2025-09-30 00:09:36 +0530

    Delete ArrayListPracticeProgram directory

--- ArrayListPracticeProgram/src/FindEvenOdd.java
+++
@@ -1,8 +0,0 @@
-public class FindEvenOdd {
-    public static void main(String[] args) {
-        int a = 10;
-        //checking given number is even or odd
-        String num = (a % 2 == 0) ? "Even" : "Odd";
-        System.out.println(" a : " + a + " is " + num);
-    }
-}
